d4246bcaabbe81d96c810212c7eea02c8d4e25f9
Adrian Lodder <lodder.adrian@gmail.com>
2017-04-25 20:52:52 -0700

    Address controls added

--- PromiseData/Controllers/AddressController.cs
+++ PromiseData/Controllers/AddressController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PromiseData.Models;
+using PromiseData.ViewModels;
 
 namespace PromiseData.Controllers
 {
@@ -16,16 +17,86 @@ namespace PromiseData.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
+        [HttpGet]
         public ActionResult Create()
         {
-            
-            return View();
+            var viewModel = new AddressViewModel
+            {
+                AddressTypes = _context.Code_AddressType.ToList(),
+                States = _context.LU_State.ToList()
+            };
+            return View(viewModel);
         }
 
-        public ActionResult Details(int addressID)
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(AddressViewModel viewModel)
         {
-            var address = _context.Addresses.Where(a => a.ID == addressID);
+
+            if (!ModelState.IsValid)
+            {
+                viewModel.AddressTypes = _context.Code_AddressType.ToList();
+                viewModel.States = _context.LU_State.ToList();
+                return View("Create", viewModel);
+            }
+
+
+            var address = new Address
+            {
+                AddressType_ID = viewModel.AddressType_ID,
+                Address1 = viewModel.Address1,
+                Address2 = viewModel.Address2,
+                Address3 = viewModel.Address3,
+                City = viewModel.City,
+                State_ID = viewModel.State_ID,
+                ZipCode = viewModel.ZipCode,
+                County = viewModel.County
+            };
+
+            _context.Addresses.Add(address);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Address");
+        }
+
+        [Authorize]
+        public ActionResult Details(int id)
+        {
+            var address = _context.Addresses.Single(a => a.ID == id);
             return View( address);
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            var address = _context.Addresses.Single(a => a.ID == id);
+            var viewModel = new AddressViewModel
+            {
+                AddressType_ID = (int)address.AddressType_ID,
+                Address1 = address.Address1,
+                Address2 = address.Address2,
+                Address3 = address.Address3,
+                City = address.City,
+                State_ID = address.State_ID,
+                ZipCode = address.ZipCode,
+                County = address.County,
+                AddressTypes = _context.Code_AddressType.ToList(),
+                States = _context.LU_State.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(int id)
+        {
+            var address = _context.Addresses.Single(a => a.ID == id);
+            _context.Addresses.Remove( address);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Address");
         }
 
         // GET: Address
---
+++ PromiseData/ViewModels/AddressViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using PromiseData.Models;
+
+namespace PromiseData.ViewModels
+{
+    public class AddressViewModel
+    {
+        public int ID { get; set; }
+
+        public int AddressType_ID { get; set; }
+
+        public IEnumerable<Code_AddressType> AddressTypes { get; set; }
+
+        [Required]
+        [DisplayName("Address")]
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string Address3 { get; set; }
+
+        public string City { get; set; }
+
+        public string State_ID { get; set; }
+
+        public IEnumerable<LU_State> States { get; set; }
+
+        public string ZipCode { get; set; }
+
+        [Required]
+        [DisplayName("County")]
+        public string County { get; set; }
+
+        public virtual Code_AddressType Code_AddressType { get; set; }
+
+        public virtual LU_State LU_State { get; set; }
+    }
+}
