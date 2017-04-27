2124af3fc5ae8f564b04fa0de068d477e01b79fd
Adrian Lodder <lodder.adrian@gmail.com>
2017-04-26 20:53:55 -0700

    Base Classroom controls added

--- PromiseData/Controllers/ClassroomController.cs
+++ PromiseData/Controllers/ClassroomController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PromiseData.ViewModels;
 
 namespace PromiseData.Controllers
 {
@@ -16,12 +17,103 @@ namespace PromiseData.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var viewModel = new ClassroomViewModel
+            {
+                Facilities = _context.Facilities,
+                SessionTypes = _context.Code_ProgramSessionType,
+                Services = _context.Services
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Classroom viewModel)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("Create", viewModel);
+            }
+
+            var classroom = new Classroom
+            {
+                Facility_ID = viewModel.Facility_ID,
+                Program_ID = viewModel.Program_ID,
+                ProgramSessionType_ID = viewModel.Program_ID,
+                NewOrExpandedClass = viewModel.NewOrExpandedClass,
+                SessionHours = viewModel.SessionHours,
+                SessionWeeks = viewModel.SessionWeeks,
+                PPStudents = viewModel.PPStudents,
+                NonPPStudentsHSOPK = viewModel.NonPPStudentsHSOPK,
+                NonPPStudentsThirdParty = viewModel.NonPPStudentsThirdParty,
+                NonPPStudentsParentPay = viewModel.NonPPStudentsParentPay,
+                PPSlotsUnfilled = viewModel.PPSlotsUnfilled,
+                CLASSScore_EmotionalSupport = viewModel.CLASSScore_EmotionalSupport,
+                CLASSScore_ClassroomOrganization = viewModel.CLASSScore_ClassroomOrganization,
+                CLASSScore_InstructionalSupport = viewModel.CLASSScore_InstructionalSupport,
+                upsize_ts = viewModel.upsize_ts
+            };
+
+            _context.Classrooms.Add(classroom);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Classroom");
+        }
+
+        [Authorize]
+        public ActionResult Details(int id)
+        {
+            var classroom = _context.Classrooms.Single(a => a.ID == id);
+            return View(classroom);
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            var classroom = _context.Classrooms.Single(a => a.ID == id);
+            var viewModel = new Classroom
+            {
+                Facility_ID = classroom.Facility_ID,
+                Program_ID = classroom.Program_ID,
+                ProgramSessionType_ID = classroom.Program_ID,
+                NewOrExpandedClass = classroom.NewOrExpandedClass,
+                SessionHours = classroom.SessionHours,
+                SessionWeeks = classroom.SessionWeeks,
+                PPStudents = classroom.PPStudents,
+                NonPPStudentsHSOPK = classroom.NonPPStudentsHSOPK,
+                NonPPStudentsThirdParty = classroom.NonPPStudentsThirdParty,
+                NonPPStudentsParentPay = classroom.NonPPStudentsParentPay,
+                PPSlotsUnfilled = classroom.PPSlotsUnfilled,
+                CLASSScore_EmotionalSupport = classroom.CLASSScore_EmotionalSupport,
+                CLASSScore_ClassroomOrganization = classroom.CLASSScore_ClassroomOrganization,
+                CLASSScore_InstructionalSupport = classroom.CLASSScore_InstructionalSupport,
+                upsize_ts = classroom.upsize_ts
+            };
+            return View( viewModel);
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public ActionResult ConfirmDelete(int id)
+        {
+            var classroom = _context.Classrooms.Single(a => a.ID == id);
+            _context.Classrooms.Remove(classroom);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Classroom");
+        }
+
         // GET: Classroom
         public ActionResult Index()
         {
-            var students = _context.Children;
-            var classroom = _context.Classrooms;
-            return View();
+            var classrooms = _context.Classrooms;
+            return View( classrooms);
         }
     }
 }
---
+++ PromiseData/ViewModels/ClassroomViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PromiseData.Models;
+
+namespace PromiseData.ViewModels
+{
+    public class ClassroomViewModel
+    {
+        public int ID { get; set; }
+
+        public int Facility_ID { get; set; }
+
+        public int Program_ID { get; set; }
+
+        public int ProgramSessionType_ID { get; set; }
+
+        public string NewOrExpandedClass { get; set; }
+
+        public double SessionHours { get; set; }
+
+        public int SessionDays { get; set; }
+
+        public int SessionWeeks { get; set; }
+
+        public int PPStudents { get; set; }
+
+        public int NonPPStudentsHSOPK { get; set; }
+
+        public int NonPPStudentsThirdParty { get; set; }
+
+        public int NonPPStudentsParentPay { get; set; }
+
+        public int PPSlotsUnfilled { get; set; }
+
+        public int CLASSScore_EmotionalSupport { get; set; }
+
+        public int CLASSScore_ClassroomOrganization { get; set; }
+
+        public int CLASSScore_InstructionalSupport { get; set; }
+
+        public byte[] upsize_ts { get; set; }
+
+        public IEnumerable<Facility> Facilities { get; set; }
+
+        public IEnumerable<Code_ProgramSessionType> SessionTypes { get; set; }
+
+        public IEnumerable<Service> Services { get; set; }
+    }
+}
