2dfd66d5c8ee401908a7562726fa1ed989949fb5
Adrian Lodder <lodder.adrian@gmail.com>
2017-04-28 19:57:40 -0700

    Added facility form

---
+++ PromiseData/Controllers/FacilityController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PromiseData.Models;
+using PromiseData.ViewModels;
+
+namespace PromiseData.Controllers
+{
+    public class FacilityController : Controller
+    {
+        private ApplicationDbContext _context;
+        private List<String> FacilityTypes;
+
+        public FacilityController()
+        {
+            _context = new ApplicationDbContext();
+            FacilityTypes = new List<string>();
+            FacilityTypes.Add("Registered Family");
+            FacilityTypes.Add("Certified Family");
+            FacilityTypes.Add("Certified Center");
+            FacilityTypes.Add("Relief Nursery");
+            FacilityTypes.Add("Private Preschool");
+            FacilityTypes.Add("Public School");
+            FacilityTypes.Add("ESD");
+            FacilityTypes.Add("CBO w/ preschool");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var viewModel = new FacilityViewModel
+            {
+                FacilityTypes = this.FacilityTypes,
+                SupportTypes = _context.Code_AdditionalSupportTypes
+            };
+            return View(viewModel);
+        }
+
+        // GET: Facility
+        public ActionResult Index()
+        {
+            var viewModel = _context.Facilities;
+            return View( viewModel);
+        }
+    }
+}
---
+++ PromiseData/ViewModels/FacilityViewModel.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PromiseData.Models;
+
+namespace PromiseData.ViewModels
+{
+
+    public class FacilityViewModel
+    {
+        public int ID { get; set; }
+
+        public string ProviderFacilityType { get; set; }
+        public List<String> FacilityTypes { get; set; }
+
+        public string Turnover_NonPPStaff { get; set; }
+
+        public string TurnoverReasons_NonPPStaff { get; set; }
+
+        public bool Transportation_services_offered { get; set; }
+
+        public int ChildrenReceivingTransportationServices { get; set; }
+
+        public int AdditionalChildFamilySupports_ID { get; set; }
+        public IEnumerable<Code_AdditionalSupportTypes> SupportTypes { get; set; }
+
+        public DateTime MonitoringVisit1Date { get; set; }
+
+        public string MonitoringVisit1Result { get; set; }
+
+        public DateTime MonitoringVisit2Date { get; set; }
+
+        public string MonitoringVisit2Result { get; set; }
+    }
+}
