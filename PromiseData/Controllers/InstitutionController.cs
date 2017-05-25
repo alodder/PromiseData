f801e0f33b7a07e34062cb42c701f41ffebcb7d9
Adrian Lodder <lodder.adrian@gmail.com>
2017-05-25 14:32:25 -0700

    Institute controller SaveChanges()

--- PromiseData/Controllers/InstitutionController.cs
+++ PromiseData/Controllers/InstitutionController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PromiseData.Models;
+using PromiseData.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +10,50 @@ namespace PromiseData.Controllers
 {
     public class InstitutionController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public InstitutionController()
+        {
+            _context = new ApplicationDbContext();
+        }
 
         // GET: Institution
         [HttpGet]
         public ActionResult Create()
         {
-            return View();
+            var ViewModel = new InstitutionViewModel();
+            return View(ViewModel);
+        }
+
+        // GET: Institution
+        [HttpPost]
+        public ActionResult Create(InstitutionViewModel viewModel)
+        {
+            var ViewModel = new InstitutionViewModel();
+
+            viewModel.DirectorAgent = _context.ContactAgents.Add(viewModel.DirectorAgent);
+            viewModel.ContactAgent = _context.ContactAgents.Add(viewModel.DirectorAgent);
+
+            var institute = new Institution {
+                LegalName = viewModel.LegalName,
+                Region = viewModel.Region,
+                BackboneOrg = viewModel.BackboneOrg,
+                WebAddress = viewModel.WebAddress,
+                DirectorAgentId = viewModel.DirectorAgent.AgentId,
+                ContactAgentId = viewModel.ContactAgent.AgentId,
+                LocationAddressId = 0,
+                MailingAddressId = 0,
+                ActiveDate = viewModel.ActiveDate,
+                EndDate = viewModel.EndDate,
+                isHub = viewModel.isHub,
+                isProvider = viewModel.isProvider
+            };
+
+            _context.Institutions.Add(institute);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Institution");
         }
 
         // GET: Institution
---
+++ PromiseData/ViewModels/InstitutionViewModel.cs
@@ -0,0 +1,47 @@
+﻿using PromiseData.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PromiseData.ViewModels
+{
+    public class InstitutionViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string LegalName { get; set; }
+
+        public string Region { get; set; }
+
+        public string BackboneOrg { get; set; }
+
+        public string WebAddress { get; set; }
+
+        public int? DirectorAgentId { get; set; }
+
+        public int? ContactAgentId { get; set; }
+
+        public int? LocationAddressId { get; set; }
+
+        public int? MailingAddressId { get; set; }
+
+        public DateTime ActiveDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public bool isHub { get; set; }
+
+        public bool isProvider { get; set; }
+
+        public virtual ContactAgent DirectorAgent { get; set; }
+
+        public virtual ContactAgent ContactAgent { get; set; }
+
+        public virtual Address AddressMail { get; set; }
+
+        public virtual Address AddressPhysical { get; set; }
+    }
+}
